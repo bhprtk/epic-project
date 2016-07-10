c694b52c5b62283ccade4a571e10e760fc757585
pratik <lazy.monk.13@gmail.com>
2016-07-09 18:24:23 -0700

    figure out getCurrentUser

--- js/components/ProfileComponent/CoverComponent.js
+++ js/components/ProfileComponent/CoverComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class CoverComponent extends React.Component {
 	render() {
+
 		return (
 			<h1>CoverComponent</h1>
 		)
--- js/components/ProfileComponent/ProfileComponent.js
+++ js/components/ProfileComponent/ProfileComponent.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import CoverComponent from './CoverComponent';
+import UserStore from '../../stores/UserStore';
 
 export default class ProfileComponent extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			currentUser: UserStore.getCurrentUser()
+		}
 	}
 
+	componentDidMount() {
+		console.log('this.state', this.state);
+	}
 
 	render() {
+		console.log('this.props', this.props);
 		return (
 			<CoverComponent />
 		)
