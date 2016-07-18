31b1efb3da276aa75851fbbea3e6001be5a41916
pratik <lazy.monk.13@gmail.com>
2016-07-17 19:34:52 -0700

    add info picture

--- js/components/MainComponent/InfoComponent.js
+++ js/components/MainComponent/InfoComponent.js
@@ -5,7 +5,24 @@ export default class InfoComponent extends React.Component {
 	render() {
 		return(
 			<div style={styles.container}>
-				<h1>hello</h1>
+
+				<div className="text-center" style={styles.banner}>
+					<div className="container">
+
+						<div className="col-md-4 col-sm-4">
+								<div>
+									<img src="./employee.png" className="img-circle"/>
+
+								</div>
+								<div className="caption">
+									<h3>yup</h3>
+								</div>
+
+						</div>
+
+					</div>
+
+				</div>
 
 			</div>
 		)
@@ -15,5 +32,12 @@ export default class InfoComponent extends React.Component {
 const styles = {
 	container: {
 		height: '100vh',
-	}
+	},
+	banner: {
+		// height: '30vh',
+		background: '#222',
+		color: '#fff',
+		marginTop: 100,
+		paddingTop: 70
+	},
 }
